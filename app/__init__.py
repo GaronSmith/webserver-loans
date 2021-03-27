9e42d4088ccf1d059136740e239b7f3231a200fd
GaronSmith <wgaronsmith13@gmail.com>
2021-03-27 11:04:03 -0400

    established the https redirect for production

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 import os 
 from flask import Flask, request, redirect
 from flask_cors import CORS
-from flask_migrate import migrate
+from flask_migrate import Migrate
 
 from .config import Config
+from .models import db
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+db.init_app(app)
 
 CORS(app)
 
