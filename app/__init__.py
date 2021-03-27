ab1c9ae935013c410f3b6ad1843455fe0db267e5
GaronSmith <wgaronsmith13@gmail.com>
2021-03-27 10:57:52 -0400

    added CORS to app - created models directory and db.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,11 @@ from flask import Flask, request, redirect
 from flask_cors import CORS
 from flask_migrate import migrate
 
+from .config import Config
+
 app = Flask(__name__)
+
+app.config.from_object(Config)
+
+CORS(app)
+
