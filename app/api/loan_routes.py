9c7ee1bc9e2d051e60cfc6233518821bcd9f9442
GaronSmith <wgaronsmith13@gmail.com>
2021-03-27 18:23:07 -0400

    removed print from loan put

--- app/api/loan_routes.py
+++ app/api/loan_routes.py
@@ -9,14 +9,13 @@ def loans(id):
     loan = Loan.query.get(id)
     if request.method == "PUT":
         payload = json.loads(request.data)
-        print("******", "amount" in payload)
         loan.amount = payload["amount"] if "amount" in payload else loan.amount
         loan.interest_rate = payload["interest_rate"] if "interest_rate" in payload else loan.interest_rate
         loan.loan_length = payload["loan_length"] if "loan_length" in payload else loan.loan_length
         loan.monthly_payment = payload["monthly_payment"] if "monthly_payment" in payload else loan.monthly_payment
         db.session.commit()
         return {"message": "Loan updated", 
-        "loan": loan.to_dict()}
+                "loan": loan.to_dict()}
     elif request.method == "DELETE":
         db.session.delete(loan)
         db.session.commit()
