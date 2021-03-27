b33c709cb930028db1cd29de7ad496414faa1733
GaronSmith <wgaronsmith13@gmail.com>
2021-03-27 11:16:03 -0400

    created loan model

---
+++ app/models/loan.py
@@ -0,0 +1,19 @@
+from .db import db
+
+class Loan(db.Model):
+    __tablename__ = "loans"
+
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable = False)
+    interst_rate = db.Column(db.Float, nullable=False)
+    loan_length = db.Column(db.Integer, nullable=False)
+    monthly_payment = db.Column(db.Float, nullable=False)
+
+    def to_dict(self):
+        return{
+            "id": self.id
+            "amount": self.amount
+            "interst_rate": self.interst_rate
+            "loan_length": self.loan_length
+            "monthly_payment": self.monthly_payment
+        }
