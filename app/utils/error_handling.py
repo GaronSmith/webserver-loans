f3c58fca1da78d0c6bf2980b273e4b602165323e
GaronSmith <wgaronsmith13@gmail.com>
2021-03-29 20:28:48 -0400

    updated error handling for put

--- app/utils/error_handling.py
+++ app/utils/error_handling.py
@@ -18,8 +18,13 @@ def check_types(payload):
         return None
 
 def check_types_with_none(payload):
-    errors = [f'Loan {key} must be a number' for key in payload if not isinstance(
-        payload[key], (int, float)) and payload[key] is not None]
+    keys = set("amount loan_length monthly_payment interest_rate".split())
+    errors = []
+    for key in payload:
+        if(key not in keys ):
+            return make_error(400, f'Key: <{key}> is not valid')
+        elif not isinstance(payload[key], (int,float)) and payload[key] is not None:
+            errors.append(f'Loan {key} must be a number')
     if(errors):
         return make_error(400, ", ".join(errors))
     else:
